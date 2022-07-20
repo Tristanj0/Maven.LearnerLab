50e3680ef90669a91d2a31b4b6355c73b7d2021d
Tristan <tristan@Zipcoders-MacBook-Pro-2.local>
2022-07-20 17:06:18 -0400

    Passed the person test

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,29 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    public final Long Id;
+
+    public String name;
+    public Person (long Id, String name) {
+        this.Id = Id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
+
+
+
+
+
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,34 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
+
+    @Test
+    public void testConstructor() {
+    //Given
+       long expectedId = 20;
+       String expectedName = "Tristan";
+    //When
+        Person test = new Person(expectedId, expectedName);
+    //Then
+        Assert.assertEquals(expectedId, test.getId());
+        Assert.assertEquals(expectedName, test.getName());
+    }
+
+    @Test
+    public void testSetName() {
+    //Given
+        String originalName = "James";
+        long Id = 15;
+    //When
+        Person test = new Person(Id, originalName);
+        String expectedName = "Tristan";
+        test.setName(expectedName);
+    //Then
+        Assert.assertEquals(expectedName, test.getName());
+
+    }
 
 }
